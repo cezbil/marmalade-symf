81cf4accd9baf1b31d9279cd3c6a42c88e816b65
cezbil <czaro93pl@gmail.com>
2019-12-05 01:35:06 +0000

    working version

---
+++ quotation.sql
@@ -0,0 +1,97 @@
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE DATABASE IF NOT EXISTS `quotation`;
+
+USE quotation;
+
+-- ----------------------------
+--  Table structure for `abi_code_rating`
+-- ----------------------------
+DROP TABLE IF EXISTS `abi_code_rating`;
+CREATE TABLE `abi_code_rating` (
+  `abi_code` varchar(10) NOT NULL,
+  `rating_factor` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`abi_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+--  Records of `abi_code_rating`
+-- ----------------------------
+BEGIN;
+INSERT INTO `abi_code_rating` VALUES ('22529902', '0.95'), ('46545255', '1.15'), ('52123803', '1.20');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `age_rating`
+-- ----------------------------
+DROP TABLE IF EXISTS `age_rating`;
+CREATE TABLE `age_rating` (
+  `age` int(11) NOT NULL,
+  `rating_factor` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`age`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+--  Records of `age_rating`
+-- ----------------------------
+BEGIN;
+INSERT INTO `age_rating` VALUES ('17', '1.50'), ('18', '1.40'), ('19', '1.30'), ('20', '1.20'), ('21', '1.10'), ('22', '1.00'), ('23', '0.95'), ('24', '0.90'), ('25', '0.75');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `base_premium`
+-- ----------------------------
+DROP TABLE IF EXISTS `base_premium`;
+CREATE TABLE `base_premium` (
+  `base_premium` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+--  Records of `base_premium`
+-- ----------------------------
+BEGIN;
+INSERT INTO `base_premium` VALUES ('500.00');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `postcode_rating`
+-- ----------------------------
+DROP TABLE IF EXISTS `postcode_rating`;
+CREATE TABLE `postcode_rating` (
+  `postcode_area` varchar(4) NOT NULL,
+  `rating_factor` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`postcode_area`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+--  Records of `postcode_rating`
+-- ----------------------------
+BEGIN;
+INSERT INTO `postcode_rating` VALUES ('LE10', '1.35'), ('PE3', '1.10'), ('WR2', '0.90');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `quote`
+-- ----------------------------
+DROP TABLE IF EXISTS `quote`;
+CREATE TABLE `quote` (
+  `id` int(11) NOT NULL,
+  `policy_number` varchar(20) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `postcode` varchar(10) DEFAULT NULL,
+  `reg_no` varchar(10) DEFAULT NULL,
+  `abi_code` varchar(10) DEFAULT NULL,
+  `premium` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+--  I had to run some sql, please see below
+-- ----------------------------
+SET GLOBAL sql_mode=''
+
+alter table quote modify id int auto_increment;
--- src/Controller/Premium/PremiumController.php
+++ src/Controller/Premium/PremiumController.php
@@ -3,36 +3,107 @@
 namespace App\Controller\Premium;
 
 use App\Entity\AbiCodeRating;
+use App\Entity\AgeRating;
+use App\Entity\BasePremium;
+use App\Entity\PostcodeRating;
+use App\Entity\Quote;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpClient\HttpClient;
-use GuzzleHttp\Client;
 
 class PremiumController extends Controller
 {
     public function getPremium(Request $request)
     {
+        $basePremium = $this->getBasePremium();
         $client = HttpClient::create();
         if ($request->headers->get('Content-Type') === 'application/json') {
             $data = $this->getData($request->getContent());
             $response = $client->request('GET', 'https://marm-symfony-api.firebaseio.com/' . $data['regNo'] . '.json');
-            var_dump($response->getContent());
-            die();
-            $decodedPayload = $response->toArray();
-            print_r($decodedPayload);
+            $abiCode = $response->getContent();
+            $ageRating = $this->getAgeRating($data['age']);
+            $postcodeRating = $this->getPostcodeRating($data['postcode']);
+            $abiCodeRating = $abiCode !== 'null' ? $this->getAbiRating($abiCode) : 1;
+            $premium = $this->calculatePremium($basePremium, $ageRating, $postcodeRating, $abiCodeRating);
+            $quote = $this->createQuote($premium, $data['age'], $data['full_postcode'], $abiCode, $data['regNo']);
         }
+        $responseData = [
+            'quote_id' => $quote->getId(),
+            'policy_number' => $quote->getPolicyNumber(),
+            'premium' => $quote->getPremium()
+        ];
 
-        return new JsonResponse($data);
+        return new JsonResponse($responseData);
     }
 
     private function getData($content)
     {
         $data = json_decode($content, true);
         $data['age'] = isset($data['age']) && $data['age'] !== '' ? $data['age'] : '';
-        $data['postcode'] = isset($data['postcode']) && $data['postcode'] !== '' ? $data['postcode'] : '';
-        $data['regNo'] = isset($data['regNo']) && $data['regNo'] !== '' ? $data['regNo'] : '';
+        $data['full_postcode'] = isset($data['postcode']) && $data['postcode'] !== '' ? $this->trimInputs($data['postcode']) : '';
+        $data['postcode'] = isset($data['postcode']) && $data['postcode'] !== '' ? $this->getFirstPart(strtoupper($data['postcode'])) : '';
+        $data['regNo'] = isset($data['regNo']) && $data['regNo'] !== '' ? strtoupper($this->trimInputs($data['regNo'])) : '';
 
         return $data;
     }
+
+    private function getBasePremium()
+    {
+        $basePremiumArr = $this->getDoctrine()->getRepository(BasePremium::class)->findAll();
+        $basePremiumObj = $basePremiumArr[0];
+        return $basePremiumObj->getBasePremium();
+    }
+
+    private function getAgeRating($age)
+    {
+        $ageObj = $this->getDoctrine()->getRepository(AgeRating::class)->find($age);
+        return $ageObj !== null ? $ageObj->getRatingFactor() : 1;
+    }
+
+    private function getPostcodeRating($postcode)
+    {
+        $postcodeObj = $this->getDoctrine()->getRepository(PostcodeRating::class)->find($postcode);
+        return $postcodeObj !== null ? $postcodeObj->getRatingFactor() : 1;
+    }
+
+    private function getAbiRating($abiCode)
+    {
+        $abiCodeObj = $this->getDoctrine()->getRepository(AbiCodeRating::class)->find($abiCode);
+        return $abiCodeObj->getRatingFactor();
+    }
+
+    private function trimInputs($input)
+    {
+        return preg_replace('/\s+/', '', $input);
+    }
+
+    function getFirstPart($postcode)
+    {
+        if (preg_match('/^[A-Z]([A-Z]?\d(\d|[A-Z])?|\d[A-Z]?)\s*?\d[A-Z][A-Z]$/i', $postcode)) {
+            return preg_replace('/^([A-Z]([A-Z]?\d(\d|[A-Z])?|\d[A-Z]?))\s*?(\d[A-Z][A-Z])$/i', '$1', $postcode);
+        }
+    }
+    private function calculatePremium($basePremium, $ageRating, $postcodeRating, $abiRating)
+    {
+        return $basePremium * $ageRating * $postcodeRating * $abiRating;
+    }
+
+    private function createQuote($premium, $age, $postcode, $abiCode, $regNum)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $quote = new Quote();
+        $quote->setPolicyNumber(rand(0, 999999999999999999));
+        $quote->setAbiCode($abiCode);
+        $quote->setRegNo($regNum);
+        $quote->setAge($age);
+        $quote->setPostcode($postcode);
+        $quote->setPremium($premium);
+        $entityManager->persist($quote);
+        $entityManager->flush();
+
+        return $quote;
+    }
+
 }
